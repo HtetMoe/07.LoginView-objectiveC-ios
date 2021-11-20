49c7a2c37b2c968109c67af0a3bbdc51531a3fa3
HtetMoePhyu <htetmoephyu.mhs@gmail.com>
2021-11-20 11:19:10 +0630

    Initial Commit

---
+++ LoginView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LoginView
+//
+//  Created by Htet Moe Phyu on 20/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LoginView/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LoginView
+//
+//  Created by Htet Moe Phyu on 20/11/2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
